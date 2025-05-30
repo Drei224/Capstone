d688c485494c92e8e108827803a0c746c4e219c6
Drei224 <ronnrodriguez0824@gmail.com>
2025-05-30 21:27:15 +0800

    Update FindCourse.php

--- UserInterface/pages/FindCourse.php
+++ UserInterface/pages/FindCourse.php
@@ -1,10 +1,81 @@
-<!DOCTYPE html>
+<<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <script src="../../js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../Assets/style.css">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <title>About Us</title>
+
+    <style>
+        body{
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-image: url("../../Assets/bg.jpg");
+        }
+
+        .card{
+            /* From https://css.glass */
+            background: rgba(255, 255, 255, 0.05);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(3.3px);
+            -webkit-backdrop-filter: blur(3.3px);
+            border: 1px solid rgba(255, 255, 255, 0.07);
+        }
+        #our_team{
+            background-color: #190960;
+        }
+    </style>
 </head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../../Assets/nc_finder_logo_transparent.png" alt="avatar">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto justify-content-end  mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a style="color: #190960" class="nav-link fw-bold" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: #190960" class="nav-link fw-bold" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: #190960" class="nav-link fw-bold" href="contact_us.php">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a style="color: #190960" class="nav-link fw-bold" href="about_us.php">About Us</a>
+                    </li>
+                </ul>
+                <div class="d-flex align-items-center justify-content-end p-2">
+                    <div class="dropdown">
+                        <a class="btn btn-light d-flex align-items-center rounded-circle p-0 border-0" href="#" role="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false" style="width: 44px; height: 44px;">
+                            <i class="fa-solid fa-user-tie text-secondary m-auto"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end mt-2 shadow" aria-labelledby="userMenu">
+                            <li><a class="dropdown-item" href="#"><i class="fa fa-user me-2"></i>Profile</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fa fa-cog me-2"></i>Settings</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item text-danger" href="#"><i class="fa fa-sign-out-alt me-2"></i>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
 <body>
     
 </body>
